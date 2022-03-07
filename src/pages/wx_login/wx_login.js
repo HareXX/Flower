bc03150e5c77a052c0f645f4cd1abb3a47c0e005
HareXX <2260122782@qq.com>
2022-03-07 22:58:58 +0800

    upd wx_login.js

--- src/pages/wx_login/wx_login.js
+++ src/pages/wx_login/wx_login.js
@@ -22,18 +22,31 @@ Page({
           let picture=user.avatarUrl;      
           // this.setData({
           //   userInfo:user,
-          //       })
+		  //       })
+		  var id
                 wx.login({
                   success (res) {
                     if (res.code) {
                       console.log(res.code)
                       console.log(picture)
-                      console.log(wxname)
+					  console.log(wxname)
+					  wx.request({
+						     url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx1aa63d33e9daf0e6&secret=71d7bed7695d61d4f896230701f4a094&js_code=' + res.code + '&grant_type=authorization_code',
+						     success: res => {
+						      // 获取到用户的 openid
+						      console.log("用户的openid:" + res.data.openid);
+						      id=res.data.openid
+						      wx.setStorageSync('id',id)
+						      that.setData({
+						        identity:id
+						            })
+						     }
+						    });  
                       //发起网络请求
                       wx.request({
                         url: 'http://47.113.191.64:9001/user/login',
                         data: {
-                          code:res.code,
+                          code:id,
                           name:wxname,
                           picture:picture
                         }, 
@@ -41,7 +54,7 @@ Page({
 
                       // 携带的参数会以url格式传到服务器，信息头我们设置为url编码，utf8编码
                       header: {
-                        'content-type': 'application/x-www-form-urlencoded;charset=utf-8' 
+                        'content-type': 'application/json;charset=utf-8' 
                       },
                          success: function (res) {
                         console.log(res.data)

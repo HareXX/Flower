ea30be6f9318b27e546afc6c099abd19cf612226
Morgan <1377060711@qq.com>
2022-03-15 19:21:54 +0800

    pie

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -3,8 +3,6 @@ var wxCharts = require('../../utils/wxcharts.js');
 var lineChart = null;
 var pieChart = null;
 Page({
-
-
 	data: {
 		hasInvestment: true,
 	},
@@ -42,9 +40,6 @@ Page({
 			url: '../smart_invest/smart_invest',
 		})
 	},
-	touchHandler: function (e) {
-		console.log(pieChart.getCurrentDataIndex(e));
-	},
 	onLoad: function (e) {
 		var windowWidth = 320;
 		try {
@@ -72,78 +67,6 @@ Page({
 			height: 300,
 			dataLabel: true,
 		});
-        lineChart = new wxCharts({
-            canvasId: 'lineCanvas',
-            type: 'line',
-            categories: simulationData.categories,
-            animation: true,
-            // background: '#f5f5f5',
-            series: [{
-                name: '成交量1',
-                data: simulationData.data,
-                format: function (val, name) {
-                    return val.toFixed(2) + '万';
-                }
-            }, {
-                name: '成交量2',
-                data: [2, 0, 0, 3, null, 4, 0, 0, 2, 0],
-                format: function (val, name) {
-                    return val.toFixed(2) + '万';
-                }
-            }],
-            xAxis: {
-                disableGrid: true
-            },
-            yAxis: {
-                title: '成交金额 (万元)',
-                format: function (val) {
-                    return val.toFixed(2);
-                },
-                min: 0
-            },
-            width: windowWidth,
-            height: 200,
-            dataLabel: false,
-            dataPointShape: true,
-            extra: {
-                lineStyle: 'curve'
-            }
-        });
 	},
-	touchHandler: function (e) {
-        console.log(lineChart.getCurrentDataIndex(e));
-        lineChart.showToolTip(e, {
-            // background: '#7cb5ec',
-            format: function (item, category) {
-                return category + ' ' + item.name + ':' + item.data 
-            }
-        });
-    },    
-    createSimulationData: function () {
-        var categories = [];
-        var data = [];
-        for (var i = 0; i < 10; i++) {
-            categories.push('2016-' + (i + 1));
-            data.push(Math.random()*(20-10)+10);
-        }
-        // data[4] = null;
-        return {
-            categories: categories,
-            data: data
-        }
-    },
-    updateData: function () {
-        var simulationData = this.createSimulationData();
-        var series = [{
-            name: '成交量1',
-            data: simulationData.data,
-            format: function (val, name) {
-                return val.toFixed(2) + '万';
-            }
-        }];
-        lineChart.updateData({
-            categories: simulationData.categories,
-            series: series
-        });
-    },
+
 })

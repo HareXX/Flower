60a42a5481fed3b3b66dea9b5bf3fe384c0b8c2c
2581871230@qq.com <2581871230@qq.com>
2022-03-15 09:58:38 +0800

    追加投资

--- src/pages/add_invest/add_invest.js
+++ src/pages/add_invest/add_invest.js
@@ -5,14 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    myasset:'',
+    yester:'',
+    sumasset:'',
+    money:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that=this
+    var tempid=wx.getStorageSync('id')
+    wx.request({
+      url: 'http://localhost:9001/asset/financial',
+      data: {
+        identity:tempid,
+      }, 
+      method: 'POST',
+    // 携带的参数会以url格式传到服务器，信息头我们设置为url编码，utf8编码
+    header: {'content-type': 'application/x-www-form-urlencoded'},
+       success: function (res) {
+      console.log(res.data.assets)
+      console.log(res.data.benefitsSum)
+      console.log(res.data.benefitsYesterday)
+     var myasset=res.data.assets
+     var sumasset=res.data.benefitsSum
+     var yester=res.data.benefitsYesterday
+     that.setData({
+      myasset:myasset,
+      yester:yester,
+      sumasset:sumasset
+    })
+     }
+    })
   },
 
   /**
@@ -62,5 +88,30 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onSubmit: function(e){
+    var amount=e.detail.value.money
+    console.log(amount)
+    var tempid=wx.getStorageSync('id')
+    console.log(tempid)
+    wx.request({
+      url: 'http://localhost:9001/invest/add',
+      data: {
+        identity:tempid,
+        amount:amount
+      }, 
+      method: 'POST',
+      header: {'content-type': 'application/json;charset=UTF-8'},
+      success: function (res) {
+        console.log(res.data)
+        wx.switchTab({
+          url: '../invest/invest',
+        })
+      },
+      fail:function(err){
+        console.log("失败")
+    }
+    })
   }
 })

8bc39833d6d4885de87823aec84f09386f9720cc
Morgan <1377060711@qq.com>
2022-03-15 19:56:07 +0800

    line

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -66,7 +66,81 @@ Page({
 			width: windowWidth,
 			height: 300,
 			dataLabel: true,
-		});
-	},
+        });
+        var simulationData = this.createSimulationData();
+        lineChart = new wxCharts({
+            canvasId: 'lineCanvas',
+            type: 'line',
+            categories: simulationData.categories,
+            animation: true,
+            // background: '#f5f5f5',
+            series: [{
+                name: '股票',
+                data: simulationData.data,
+                format: function (val, name) {
+                    return val.toFixed(2) + '万';
+                }
+            }, {
+                name: '债券',
+                data: [2, 0, 0, 3, 5, 4, 4, 5, 6, 5],
+                format: function (val, name) {
+                    return val.toFixed(2) + '万';
+                }
+            }],
+            xAxis: {
+                disableGrid: true
+            },
+            yAxis: {
+                title: '收益金额 (万元)',
+                format: function (val) {
+                    return val.toFixed(2);
+                },
+                min: 0
+            },
+            width: windowWidth,
+            height: 200,
+            dataLabel: false,
+            dataPointShape: true,
+            extra: {
+                lineStyle: 'curve'
+            }
+        });
+    },
+    touchHandler: function (e) {
+        console.log(lineChart.getCurrentDataIndex(e));
+        lineChart.showToolTip(e, {
+            // background: '#7cb5ec',
+            format: function (item, category) {
+                return category + ' ' + item.name + ':' + item.data 
+            }
+        });
+    },    
+    createSimulationData: function () {
+        var categories = [];
+        var data = [];
+        for (var i = 0; i < 10; i++) {
+            categories.push('2016-' + (i + 1));
+            data.push(Math.random()*(20-10)+10);
+        }
+        // data[4] = null;
+        return {
+            categories: categories,
+            data: data
+        }
+    },
+    updateData: function () {
+        var simulationData = this.createSimulationData();
+        var series = [{
+            name: '成交量1',
+            data: simulationData.data,
+            format: function (val, name) {
+                return val.toFixed(2) + '万';
+            }
+        }];
+        lineChart.updateData({
+            categories: simulationData.categories,
+            series: series
+        });
+    },
 
 })
